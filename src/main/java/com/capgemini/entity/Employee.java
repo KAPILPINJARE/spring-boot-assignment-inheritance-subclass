1e9e1b46cf6cb52e598e38315e7e47850ed5f096
pawar <kapil.pawar@capgemini.com>
2019-05-16 19:27:35 +0530

    commit

---
+++ src/main/java/com/capgemini/entity/Employee.java
@@ -0,0 +1,50 @@
+package com.capgemini.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity  
+@Table(name = "employee_subclass")  
+@Inheritance(strategy=InheritanceType.JOINED)  
+public class Employee {
+	@Id  
+	private int empId;
+	private String empName;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+	}
+	
+	
+}
